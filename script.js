d7aa16ff2cdca19dc1ef7b1cfb70e9f5b985b71f
Amir <amirhamidiyan84@gmail.com>
2025-08-12 16:41:27 +0330

    add feedbacks in page with submit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const txtareaEL = document.querySelector(".form__textarea");
 const counterEl = document.querySelector(".counter");
 const formEl = document.querySelector(".form");
+const feedsEL = document.querySelector(".feedbacks");
+const submitEL = document.querySelector('.submit-btn')
 
 const inputhandler = () => {
   const typedChars = txtareaEL.value.length;
@@ -38,9 +40,30 @@ const formhandler = (event) => {
 
   const hashtag = txt.split(" ").find((word) => word.includes("#"));
   const company = hashtag.substring(1);
-  const days = 0;
-  const vote = 0;
+  const date = 0;
+  const upvote = 0;
   const letter = company.substring(0,1).toUpperCase();
+  const feedItem = `
+    <li class="feedback">
+            <button class="upvote">
+                <i class="fa-solid fa-caret-up upvote__icon"></i>
+                <span class="upvote__count">${upvote}</span>
+            </button>
+            <section class="feedback__badge">
+                <p class="feedback__letter">${letter}</p>
+            </section>
+            <div class="feedback__content">
+                <p class="feedback__company">${company}</p>
+                <p class="feedback__text">${txt}</p>
+            </div>
+            <p class="feedback__date">${date === 0 ? 'New' : `${date}d`}</p>
+        </li>
+    `;
+
+    feedsEL.insertAdjacentHTML('beforeend',feedItem);
+    txtareaEL.value ='';
+    submitEL.blur();
+    counterEl.textContent = 150;
 };
 
 formEl.addEventListener("submit", formhandler);
